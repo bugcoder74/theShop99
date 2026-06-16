f6f1ddf36fe37b150cf7acab36ebd759acdf7300
BugCoder <hdndcomputers6446@gmail.com>
2026-06-16 16:19:43 +0530

    Empty spring initializer push

---
+++ src/main/java/com/bugcoder/theShop99/TheShop99Application.java
@@ -0,0 +1,13 @@
+package com.bugcoder.theShop99;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheShop99Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheShop99Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bugcoder/theShop99/TheShop99ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugcoder.theShop99;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheShop99ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
